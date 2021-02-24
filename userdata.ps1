572b70bb05860590851e877bc0f5027c7ccb6fb8
neoof86 <edwards-gareth1@sky.com>
2021-02-24 19:44:01 +0000

    Update userdata.ps1

--- userdata.ps1
+++ userdata.ps1
@@ -23,19 +23,16 @@ New-Item -Path WSMan:\LocalHost\Listener -Transport HTTPS -Address * -Certificat
 #cmd.exe /c netsh advfirewall firewall add rule name="WinRM-SSL (5986)" dir=in action=allow protocol=TCP localport=5986
 
 Write-Output "Configuring Powershell Remoting"
+cmd.exe /c net user winrm 'Letmein12!' /add /y
+cmd.exe /c localgroup administrators winrm /add
 # Configure WinRM
 cmd.exe /c winrm quickconfig -q
 cmd.exe /c winrm set "winrm/config" '@{MaxTimeoutms="1800000"}'
 cmd.exe /c winrm set "winrm/config/winrs" '@{MaxMemoryPerShellMB="1024"}'
-cmd.exe /c winrm set "winrm/config/service" '@{AllowUnencrypted="false"}'
-cmd.exe /c winrm set "winrm/config/client" '@{AllowUnencrypted="false"}'
-cmd.exe /c winrm set "winrm/config/service/auth" '@{Basic="true"}'
-cmd.exe /c winrm set "winrm/config/client/auth" '@{Basic="true"}'
-cmd.exe /c winrm set "winrm/config/service/auth" '@{CredSSP="true"}'
+cmd.exe /c winrm set winrm/config/service '@{AllowUnencrypted="true"}'
+cmd.exe /c winrm set winrm/config/service/auth '@{Basic="true"}'
 cmd.exe /c winrm set "winrm/config/listener?Address=*+Transport=HTTPS" "@{Port=`"5986`";Hostname=`"packer-ami-builder`";CertificateThumbprint=`"$($Cert.Thumbprint)`"}"
 cmd.exe /c netsh advfirewall firewall add rule name="WinRM-SSL (5986)" dir=in action=allow protocol=TCP localport=5986
-cmd.exe /c net user winrm 'Letmein12!' /add /y
-cmd.exe /c localgroup administrators winrm /add
 cmd.exe /c net stop winrm
 cmd.exe /c sc config winrm start= auto
 cmd.exe /c net start winrm
@@ -49,6 +46,7 @@ cmd.exe /c net start winrm
 
 #Set-Service -Name 'WinRM' -StartupType Automatic
 #Set-ItemProperty 'HKLM:\SYSTEM\CurrentControlSet\Services\WinRM' -Name 'DelayedAutoStart' -Value 0
+cmd.exe /c netsh advfirewall firewall add rule name="WinRM 5985" protocol=TCP dir=in localport=5985 action=allow
 cmd.exe /c netsh advfirewall firewall add rule name="WinRM 5985" protocol=TCP dir=in localport=5985 action=allow
 cmd.exe /c netsh advfirewall firewall add rule name="ICMP Allow incoming V4 echo request" protocol=icmpv4:8,any dir=in action=allow
 #cmd.exe /c net stop winrm
