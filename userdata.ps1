21b63e62c2700f84025d1f9ee5320349033e94a3
neoof86 <edwards-gareth1@sky.com>
2021-02-24 17:14:30 +0000

    Update userdata.ps1

--- userdata.ps1
+++ userdata.ps1
@@ -12,15 +12,15 @@ Remove-Item -Path WSMan:\Localhost\listener\listener* -Recurse
 #New-Item -Path WSMan:\LocalHost\Listener -Transport HTTPS -Address * -CertificateThumbPrint $Cert.Thumbprint -Force
  
 # Configure WinRM
-#cmd.exe /c winrm set "winrm/config" '@{MaxTimeoutms="1800000"}'
-#cmd.exe /c winrm set "winrm/config/winrs" '@{MaxMemoryPerShellMB="1024"}'
-#cmd.exe /c winrm set "winrm/config/service" '@{AllowUnencrypted="false"}'
-#cmd.exe /c winrm set "winrm/config/client" '@{AllowUnencrypted="false"}'
-#cmd.exe /c winrm set "winrm/config/service/auth" '@{Basic="true"}'
-#cmd.exe /c winrm set "winrm/config/client/auth" '@{Basic="true"}'
-#cmd.exe /c winrm set "winrm/config/service/auth" '@{CredSSP="true"}'
-#cmd.exe /c winrm set "winrm/config/listener?Address=*+Transport=HTTPS" "@{Port=`"5986`";Hostname=`"packer-ami-builder`";CertificateThumbprint=`"$($Cert.Thumbprint)`"}"
-#cmd.exe /c netsh advfirewall firewall add rule name="WinRM-SSL (5986)" dir=in action=allow protocol=TCP localport=5986
+cmd.exe /c winrm set "winrm/config" '@{MaxTimeoutms="1800000"}'
+cmd.exe /c winrm set "winrm/config/winrs" '@{MaxMemoryPerShellMB="1024"}'
+cmd.exe /c winrm set "winrm/config/service" '@{AllowUnencrypted="false"}'
+cmd.exe /c winrm set "winrm/config/client" '@{AllowUnencrypted="false"}'
+cmd.exe /c winrm set "winrm/config/service/auth" '@{Basic="true"}'
+cmd.exe /c winrm set "winrm/config/client/auth" '@{Basic="true"}'
+cmd.exe /c winrm set "winrm/config/service/auth" '@{CredSSP="true"}'
+cmd.exe /c winrm set "winrm/config/listener?Address=*+Transport=HTTPS" "@{Port=`"5986`";Hostname=`"packer-ami-builder`";CertificateThumbprint=`"$($Cert.Thumbprint)`"}"
+cmd.exe /c netsh advfirewall firewall add rule name="WinRM-SSL (5986)" dir=in action=allow protocol=TCP localport=5986
 
 Write-Output "Configuring Powershell Remoting"
 winrm quickconfig -q
