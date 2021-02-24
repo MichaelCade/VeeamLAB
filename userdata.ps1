2c4592640c0ba85809d3aae66480704a77532b0c
neoof86 <edwards-gareth1@sky.com>
2021-02-24 17:51:51 +0000

    Update userdata.ps1

--- userdata.ps1
+++ userdata.ps1
@@ -8,10 +8,23 @@ $ErrorActionPreference = "stop"
 Remove-Item -Path WSMan:\Localhost\listener\listener* -Recurse
  
 # Create self-signed cert for encrypted WinRM on port 5986
-#$Cert = New-SelfSignedCertificate -CertstoreLocation Cert:\LocalMachine\My -DnsName "packer-ami-builder"
-#New-Item -Path WSMan:\LocalHost\Listener -Transport HTTPS -Address * -CertificateThumbPrint $Cert.Thumbprint -Force
+$Cert = New-SelfSignedCertificate -CertstoreLocation Cert:\LocalMachine\My -DnsName "packer-ami-builder"
+New-Item -Path WSMan:\LocalHost\Listener -Transport HTTPS -Address * -CertificateThumbPrint $Cert.Thumbprint -Force
  
 # Configure WinRM
+#cmd.exe /c winrm set "winrm/config" '@{MaxTimeoutms="1800000"}'
+#cmd.exe /c winrm set "winrm/config/winrs" '@{MaxMemoryPerShellMB="1024"}'
+#cmd.exe /c winrm set "winrm/config/service" '@{AllowUnencrypted="false"}'
+#cmd.exe /c winrm set "winrm/config/client" '@{AllowUnencrypted="false"}'
+#cmd.exe /c winrm set "winrm/config/service/auth" '@{Basic="true"}'
+#cmd.exe /c winrm set "winrm/config/client/auth" '@{Basic="true"}'
+#cmd.exe /c winrm set "winrm/config/service/auth" '@{CredSSP="true"}'
+#cmd.exe /c winrm set "winrm/config/listener?Address=*+Transport=HTTPS" "@{Port=`"5986`";Hostname=`"packer-ami-builder`";CertificateThumbprint=`"$($Cert.Thumbprint)`"}"
+#cmd.exe /c netsh advfirewall firewall add rule name="WinRM-SSL (5986)" dir=in action=allow protocol=TCP localport=5986
+
+Write-Output "Configuring Powershell Remoting"
+# Configure WinRM
+cmd.exe /c winrm quickconfig -q
 cmd.exe /c winrm set "winrm/config" '@{MaxTimeoutms="1800000"}'
 cmd.exe /c winrm set "winrm/config/winrs" '@{MaxMemoryPerShellMB="1024"}'
 cmd.exe /c winrm set "winrm/config/service" '@{AllowUnencrypted="false"}'
@@ -21,22 +34,23 @@ cmd.exe /c winrm set "winrm/config/client/auth" '@{Basic="true"}'
 cmd.exe /c winrm set "winrm/config/service/auth" '@{CredSSP="true"}'
 cmd.exe /c winrm set "winrm/config/listener?Address=*+Transport=HTTPS" "@{Port=`"5986`";Hostname=`"packer-ami-builder`";CertificateThumbprint=`"$($Cert.Thumbprint)`"}"
 cmd.exe /c netsh advfirewall firewall add rule name="WinRM-SSL (5986)" dir=in action=allow protocol=TCP localport=5986
-
-Write-Output "Configuring Powershell Remoting"
-winrm quickconfig -q
-
-winrm set winrm/config/client/auth '@{Basic="true"}'
-winrm set winrm/config/service/auth '@{Basic="true"}'
-winrm set winrm/config/service '@{AllowUnencrypted="true"}'
-Set-Item -Force wsman:\localhost\client\trustedhosts *
-netsh advfirewall firewall add rule name="Open Port 5985" dir=in action=allow protocol=TCP localport=5985
-
-Set-Service -Name 'WinRM' -StartupType Automatic
-Set-ItemProperty 'HKLM:\SYSTEM\CurrentControlSet\Services\WinRM' -Name 'DelayedAutoStart' -Value 0
-
-netsh advfirewall firewall add rule name="ICMP Allow incoming V4 echo request" protocol=icmpv4:8,any dir=in action=allow
 cmd.exe /c net stop winrm
 cmd.exe /c sc config winrm start= auto
 cmd.exe /c net start winrm
+
+#winrm quickconfig -q
+
+#winrm set winrm/config/service/auth '@{Basic="true"}'
+#winrm set winrm/config/service '@{AllowUnencrypted="true"}'
+#Set-Item -Force wsman:\localhost\client\trustedhosts *
+#netsh advfirewall firewall add rule name="Open Port 5985" dir=in action=allow protocol=TCP localport=5985
+
+#Set-Service -Name 'WinRM' -StartupType Automatic
+#Set-ItemProperty 'HKLM:\SYSTEM\CurrentControlSet\Services\WinRM' -Name 'DelayedAutoStart' -Value 0
+
+netsh advfirewall firewall add rule name="ICMP Allow incoming V4 echo request" protocol=icmpv4:8,any dir=in action=allow
+#cmd.exe /c net stop winrm
+#cmd.exe /c sc config winrm start= auto
+c#md.exe /c net start winrm
 
 New-Item -Path "c:\" -Name "logfiles" -ItemType "directory"
