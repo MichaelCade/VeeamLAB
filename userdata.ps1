733660ee3b0e573734af2db206b34e0ac34b6a08
neoof86 <edwards-gareth1@sky.com>
2021-02-24 20:33:38 +0000

    Update userdata.ps1

--- userdata.ps1
+++ userdata.ps1
@@ -4,15 +4,15 @@ Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope LocalMachine -Force -Er
 $ErrorActionPreference = "stop"
 
 
-New-Item -Path "c:\" -Name "logfiles" -ItemType "directory"
+New-Item -Path "c:\" -Name "_veeam" -ItemType "directory"
 
 #Set Windows Firewall to OFF
 set-NetFirewallProfile -All -Enabled False
 
 #Create User and Add to Local Administrator Group
-$password = ConvertTo-SecureString 'Veeam1!' -AsPlainText -Force
-new-localuser -Name autodeploy -Password $password
-add-localgroupmember -Group administrators -Member autodeploy
+#$password = ConvertTo-SecureString 'Veeam1!' -AsPlainText -Force
+#new-localuser -Name autodeploy -Password $password
+#add-localgroupmember -Group administrators -Member autodeploy
 
 Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
 New-ItemProperty -Path "HKLM:\SOFTWARE\OpenSSH" -Name DefaultShell -Value "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -PropertyType String -Force
