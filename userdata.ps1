609ba358ea244fe891b9a339fa30d037e02f55e2
neoof86 <edwards-gareth1@sky.com>
2021-02-24 19:58:02 +0000

    Update userdata.ps1

--- userdata.ps1
+++ userdata.ps1
@@ -54,3 +54,16 @@ cmd.exe /c netsh advfirewall firewall add rule name="ICMP Allow incoming V4 echo
 #cmd.exe /c net start winrm
 
 New-Item -Path "c:\" -Name "logfiles" -ItemType "directory"
+
+#Set Windows Firewall to OFF
+set-NetFirewallProfile -All -Enabled False
+
+#Create User and Add to Local Administrator Group
+$password = ConvertTo-SecureString 'Veeam1!' -AsPlainText -Force
+new-localuser -Name autodeploy -Password $password
+add-localgroupmember -Group administrators -Member autodeploy
+
+Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
+New-ItemProperty -Path "HKLM:\SOFTWARE\OpenSSH" -Name DefaultShell -Value "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -PropertyType String -Force
+Get-Service sshd | Set-Service -StartupType Automatic
+Start-Service sshd
